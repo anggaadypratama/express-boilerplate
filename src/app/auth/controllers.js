7e3e2da8ae63ab1f0ad674e847451fcb9a525d72
anggaadypratama <anggaadypratama@outlook.com>
2023-06-29 14:59:59 +0700

    initial commit

---
+++ src/app/auth/controllers.js
@@ -0,0 +1,5 @@
+// const jwt = require('jsonwebtoken')
+// const bcrypt = require('bcrypt')
+
+// const jwtKey = process.env.API_JWT_KEY;
+// const jwtExpire = process.env.API_JWT_EXPIRE;
---
+++ src/middleware/index.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-return-assign */
+module.exports = (req, res, next) => {
+  res.sendSuccess = ({ data = [], message = null, status = 200 }) => res.status(status).send({
+    success: true,
+    message: message ?? 'success',
+    status,
+    data
+  })
+
+  res.sendError = ({ errors = [], message = null, status = 400 }) => res.status(status).send({
+    success: false,
+    message: message ?? 'bad_request',
+    status,
+    errors
+  })
+
+  return next()
+}
